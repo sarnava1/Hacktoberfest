5769b76fec362e19df2ef7aae1da5df02d0b1cde
Kumar Saurabh Sinha <sinha.ksaurabh@gmail.com>
2017-10-13 19:03:59 +0200

    formatting

--- colcha.js
+++ colcha.js
@@ -1,12 +1,10 @@
-
-
-var button=document.querySelector("button");
-var isColored=false;
-button.addEventListener("click",function(){
-	//alert('connected');
-		if(isColored)
-				document.body.style.background="#FFFFFF";
-	  else
-	   		document.body.style.background="#FFFF00";
-	     	isColored = !isColored;
+var button = document.querySelector("button");
+var isColored = false;
+button.addEventListener("click", function() {
+    //alert('connected');
+    if (isColored)
+        document.body.style.background = "#FFFFFF";
+    else
+        document.body.style.background = "#FFFF00";
+    isColored = !isColored;
 });
