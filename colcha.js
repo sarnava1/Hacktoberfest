3c7aca518c7c7c7e62169406be25849b9f31a427
koala <koga.rodrigo@gmail.com>
2017-10-13 19:03:54 -0300

    closes issue #1

--- colcha.js
+++ colcha.js
@@ -1,12 +1,11 @@
 
 
 var button=document.querySelector("button");
-var isBlue=false;
+var isYellow=false;
 button.addEventListener("click",function(){
-	//alert('connected');
-	if(isBlue)
+	if(isYellow)
 	document.body.style.background="white";
     else
-     	document.body.style.background="blue";
-     isBlue=!isBlue;
+     	document.body.style.background="yellow";
+	isYellow=!isYellow;
 });
