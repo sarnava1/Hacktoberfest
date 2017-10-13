84913a6d7bfd1e65d0d2395d2c87bb5b199df6b4
Maddie Gabriel <mgabri02@mail.uoguelph.ca>
2017-10-13 17:48:09 -0400

    changed colour to yellow!

--- colcha.js
+++ colcha.js
@@ -7,6 +7,6 @@ button.addEventListener("click",function(){
 	if(isBlue)
 	document.body.style.background="white";
     else
-     	document.body.style.background="blue";
+     	document.body.style.background="yellow";
      isBlue=!isBlue;
 });
