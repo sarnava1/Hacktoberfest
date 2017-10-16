65f2798303e934b1029b3cb4a15f73a2d9be2a0c
Ruchika Modi <ruchika.modi.apm13@itbhu.ac.in>
2017-10-17 00:25:55 +0530

    changing background to yellow
    Changing background to yellow Fixes #1

--- colcha.js
+++ colcha.js
@@ -5,7 +5,7 @@ var isBlue=false;
 button.addEventListener("click",function(){
 	//alert('connected');
 	if(isBlue)
-	document.body.style.background="white";
+	document.body.style.background="yellow";
     else
      	document.body.style.background="blue";
      isBlue=!isBlue;
