9f9f117bf744e51a76de04a8b5391ea9a0109996
u-ways <ue95.career@gmail.com>
2018-09-30 23:57:16 +0100

    Big changes - Hall of Contributors:
    Nice idea Saurav! Checkout the README.md, I have done some changes to 
    make this a nice **Hall of Contributors**!
    
    I have added some basic style, so you or anyone else afterwards can 
    style it better afterwards.
    
    #### How it works?
    It pulls the data from the data variable in core.js and append articles 
    to the `index.html`.
    
    Happy Hactoberfest!

---
+++ README.md
@@ -0,0 +1,19 @@
+# Hactoberfest 2018 - Hall of Contributors!
+
+![Screenshot](./screenshot.png)
+
+This repo was initially created when I was learning JQuery, but now this repo will serve a noble cause! It will test the power of open-source in general and Hacktoberfest in particular.
+
+#### Anyone seeing this:
+
+1. Fork this repo.
+2. Create a new branch.
+3. Go to `core.js`
+4. Scroll down and in the `data` variable:
+  - Create a new object and add your name by creating an `name` property.
+  - Add a little about yourself by creating an `about` property.
+5. commit and submit a pull request!
+
+And now you're in the open-source hall of contributors! :D
+
+---
---
+++ core.js
@@ -0,0 +1,43 @@
+/** Core js: ******************************************************************/
+
+let main = document.querySelector('#main');
+let contributorsSection = main.querySelector('#contributors');
+
+function addData (data) {
+  data.forEach(({name, about}) => {
+    name  = escapeHtml(name);
+    about = escapeHtml(about);
+
+    contributorsSection.innerHTML +=
+    '<article>' +
+      `<h3 class="name">Hello! I am <b>${name}</b>, here is a little about me:</h3>` +
+      `<p class="about">${about}</p>` +
+    '</article>';
+  });
+}
+
+/** Escape HTML special chars in JS for basic security */
+
+function escapeHtml(unsafe) {
+  return unsafe
+   .replace(/&/g, "&amp;")
+   .replace(/</g, "&lt;")
+   .replace(/>/g, "&gt;")
+   .replace(/"/g, "&quot;")
+   .replace(/'/g, "&#039;");
+ }
+
+/** Add your details below: ***************************************************/
+
+const data = [
+  {
+    "name":  "Saurav Jaiswal",
+    "about": "I am an open source enthusiast and free lancer involved in various projects and contributing to a host of open source projects. This repo was initially created when I was learning JQuery, but now this repo will serve a noble cause! It will test the power of OPEN SOURCE in GENERAL and HACKTOBERFEST in particular. ANYONE seeing this repo just comment HI with your name just as I have done. Make sure that your name is within the data array of objects and include some details about yourself! IFF you want you can STAR the repo and SPREAD it among your friends(It will help us get bigger). Let us all be friends and remain conected."
+  },
+  {
+    "name":  "U-ways",
+    "about": "I like tea and contributing to open-source!"
+  },
+];
+
+addData(data);
