fd6e2c57922de558cbee37b06b3324ab7975307a
U-ways <ue95.career@gmail.com>
2018-10-01 00:13:49 +0100

    Update core.js

--- core.js
+++ core.js
@@ -31,7 +31,7 @@ function escapeHtml(unsafe) {
 
 const data = [
   {
-    "name":  "Saurav Jaiswal",
+    "name":  "Sarnava",
     "about": "I am an open source enthusiast and free lancer involved in various projects and contributing to a host of open source projects. This repo was initially created when I was learning JQuery, but now this repo will serve a noble cause! It will test the power of OPEN SOURCE in GENERAL and HACKTOBERFEST in particular. ANYONE seeing this repo just comment HI with your name just as I have done. Make sure that your name is within the data array of objects and include some details about yourself! IFF you want you can STAR the repo and SPREAD it among your friends(It will help us get bigger). Let us all be friends and remain conected."
   },
   {
