7e1c8ca8c7f74d98c11f70031ade34b4cf84b4ad
SirJoraH21 <mr.vip.yurra90@gmail.com>
2019-10-01 23:59:13 +0300

    correct style and make random color btn

--- colcha.js
+++ colcha.js
@@ -1,10 +1,17 @@
-let button=document.querySelector("button");
-let filled=false;
-button.addEventListener("click",function(){
-	//alert('connected');
-	if(filled)
-	document.body.style.background="blue";
-    else
-     	document.body.style.background="yellow";
-     filled=!filled;
-});
+let button = document.querySelector("button");
+//let filled=false;
+//button.addEventListener("click",function(){
+//	//alert('connected');
+//	if(filled)
+//	document.body.style.background="blue";
+//    else
+//     	document.body.style.background="yellow";
+//     filled=!filled;
+//});
+button.onclick = function () {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    //document.body.style = 'rgb('r + ', ' + g + ', ' + b')';
+    document.body.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+}
